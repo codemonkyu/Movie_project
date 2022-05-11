681a264cf899427ffd32668ca494db2d7768ea5c
SeoultechTools20 <dlxo444@gmail.com>
2022-05-11 10:22:00 +0900

    fix: getMovieData

--- front-react/src/pages/MainPage/Main.js
+++ front-react/src/pages/MainPage/Main.js
@@ -6,6 +6,7 @@ import "./Main.css";
 import axios from "axios";
 
 function Main() {
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   let [bgFade, setBgFade] = useState("");
 
@@ -14,7 +15,7 @@ function Main() {
     return () => {
       setBgFade("");
     };
-  });
+  }, []);
 
   useEffect(() => {
     axios
@@ -24,46 +25,34 @@ function Main() {
         },
       })
       .then((res) => {
-        res.data.map((a) => {
-          console.log(a);
-          // return setMovies(movies.concat(a));
-        });
-        console.log(movies);
+        setMovies(movies => ([...movies, res.data]))
+        setLoading(false);
       })
-      .catch((error) => {
-        if (error.response) {
-          // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
-          console.log(localStorage.getItem("token"));
-        } else if (error.request) {
-          // 요청이 이루어 졌으나 응답을 받지 못했습니다.
-          // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
-          // Node.js의 http.ClientRequest 인스턴스입니다.
-          console.log(error.request);
-        } else {
-          // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
-  }, []);
+  }, [movies]);
 
   return (
     <div className={"mainBg-start " + bgFade}>
       <Header />
       <div className="main-body">
-        <div>
+
           {
-            movies.map((movie, i) => {
-              return (
-                <Movie
-                  id={movie[i].id}
-                  title={movie[i].title}
-                  coverImg={movie[i].poster_path}
-                />
-              );
-            })
+            loading 
+            ? <h1>Loading...</h1> 
+            :
+            
+            <div>
+              {
+                movies.map((movie, i) => {
+                  return (
+                    <Movie
+                      id={movie[i].id}
+                      title={movie[i].title}
+                      coverImg={movie[i].poster_path}
+                    />
+                  );
+              })}
+          </div>
           }
-        </div>
       </div>
     </div>
   );
