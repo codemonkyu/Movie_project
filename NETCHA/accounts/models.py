d9e9b8da8eedfb8f6ae7c6b310d40bd8c97e44ed
codemonkyu <codemonkyu@gmail.com>
2022-06-01 17:22:43 +0900

    fix: accouts model

--- NETCHA/accounts/models.py
+++ NETCHA/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
-from django.utils.translation import gettext_lazy as _
+
 
 class User(AbstractUser):
     username = None
--- NETCHA/accounts/views.py
+++ NETCHA/accounts/views.py
@@ -4,8 +4,6 @@ from rest_framework import viewsets
 from rest_framework import serializers
 
 
-
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
